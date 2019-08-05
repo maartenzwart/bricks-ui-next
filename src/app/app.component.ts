447aaed550e120e8aca2adf61723916ad12ed1c2
Max van de Laar <max.vandelaar@luminis.eu>
2019-08-05 10:44:17 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'brx-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'bricks-ui-angular';
+}
