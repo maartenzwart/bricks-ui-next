af3da839abb995c7af2b6a6fc2e230caf6993deb
Max van de Laar <max.vandelaar@luminis.eu>
2019-08-15 10:14:40 +0200

    Added header to table view

--- src/app/bricks/hour-registration/registration/table/table.component.ts
+++ src/app/bricks/hour-registration/registration/table/table.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, Inject, LOCALE_ID, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {CalendarUtils, DateAdapter, getWeekViewPeriod} from 'angular-calendar';
+import {WeekDay, CalendarEvent} from 'calendar-utils';
 
 @Component({
   selector: 'brx-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent implements OnInit {
+export class TableComponent implements OnInit, OnChanges {
+  viewDate: Date = new Date();
+  view = 'week';
+  weekStartsOn = 1;
+  excludeDays: number[] = [];
+  daysInWeek = 7;
+  weekendDays = [6, 0];
+  days: WeekDay[];
+  locale: string;
+  events: CalendarEvent[] = [];
 
-  constructor() { }
+  constructor(private cd: ChangeDetectorRef, @Inject(LOCALE_ID) locale: string, protected utils: CalendarUtils, protected dateAdapter: DateAdapter) {
+    this.locale = locale;
+  }
 
   ngOnInit() {
+    this.refreshHeader();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('CHANGEEE', changes);
+    this.refreshHeader();
+  }
+
+  refreshHeader(): void {
+    this.days = this.utils.getWeekViewHeader({
+      viewDate: this.viewDate,
+      weekStartsOn: this.weekStartsOn,
+      excluded: this.excludeDays,
+      weekendDays: this.weekendDays,
+      ...getWeekViewPeriod(
+        this.dateAdapter,
+        this.viewDate,
+        this.weekStartsOn,
+        this.excludeDays,
+        this.daysInWeek
+      )
+    });
   }
 
 }
